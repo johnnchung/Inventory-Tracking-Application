1f791e5dffac2ed957ef819688d133e79cfc1627
John Chung <johnchung03190319@gmail.com>
2022-05-17 01:58:02 -0400

    Remove config

--- config/key.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    MongoURI: "mongodb+srv://johnchung19:Rotmgnpemc11!@cluster0.dsp69.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-}
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     Inventory.find()
     .then(results => {
         res.render('index.ejs', { inventoryCollection: results })
-    }).catch(error => console.error(error))
+    })
 })
 
 module.exports = router;
