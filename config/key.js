e846babc58195d6b27eaf52f6c71dddc265ab76c
John Chung <johnchung03190319@gmail.com>
2022-05-17 02:06:50 -0400

    Fix key.js

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb+srv://johnchung19:Rotmgnpemc11!@cluster0.dsp69.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MongoURI: "mongodb+srv://johnchung19:RDJ7cpc0mllEAQ7Q@cluster0.dsp69.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
